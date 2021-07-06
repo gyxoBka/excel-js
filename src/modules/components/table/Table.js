f0bb2619f5e5080afac0111f21df7fc6b2637b0b
gyxoBka <gyxobka1996@gmail.com>
2021-07-07 00:32:13 +0300

    table resize col & rows

--- src/modules/components/table/Table.js
+++ src/modules/components/table/Table.js
@@ -1,9 +1,26 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from './table.template';
+import { resizeHandler } from './table.resize';
+import { shouldResize } from './table.functions';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
+
+    constructor($root) {
+        super($root, {
+            listeners: ['mousedown']
+        })
+
+    }
+
     toHTML() {
         return createTable(30)
+    }
+
+    onMousedown(event) {
+        if (shouldResize(event)) {
+            event.stopPropagation()
+            resizeHandler(event, this.$root)
+        }
     }
 }
